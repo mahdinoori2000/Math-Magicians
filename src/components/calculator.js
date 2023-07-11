cf4a2cfb6d87807eed6196c24f81dad817b68ad6
Mahdi Noori <mahdinoori2389@gmail.com>
2023-07-11 21:42:39 +0430

    Delete unused files

---
+++ src/components/calculator.js
@@ -0,0 +1,11 @@
+import './calculator.css';
+
+function App() {
+  return (
+    <div className="container">
+      <input className="number-input" type="text" />
+    </div>
+  );
+}
+
+export default App;
