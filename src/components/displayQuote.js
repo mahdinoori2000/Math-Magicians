5bd14aaf570385dd18f3e25e14a45f0d8c0fcf23
Mahdi Noori <mahdinoori2389@gmail.com>
2023-07-14 01:14:02 +0430

    Add displayQuote.js file to the components

---
+++ src/components/displayQuote.js
@@ -0,0 +1,7 @@
+export default function Quote() {
+  return (
+    <section className="quote">
+      <h2 className="quote">This is some randomly jenerated quote that fetched from an external API</h2>
+    </section>
+  );
+}
