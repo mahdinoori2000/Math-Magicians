ec8163c7c06a05a0a1fac3f12fc90326edd26c45
Mahdi Noori <mahdinoori2389@gmail.com>
2023-07-14 14:21:13 +0430

    Display quote in the page

--- src/components/displayQuote.js
+++ src/components/displayQuote.js
@@ -1,7 +1,38 @@
+import { useEffect, useState } from 'react';
+
 export default function Quote() {
+  const [quote, setQuote] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const displayQuote = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch('https://api.api-ninjas.com/v1/quotes?category=success', {
+          headers: { 'X-Api-Key': 'r5o8nzHQqR3Cm3JtFBGGbw==hnNgj9RXggBv2U2U' },
+          contentType: 'application/json',
+        });
+        const data = await response.json();
+        setQuote(data[0].quote);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (!quote) {
+      displayQuote();
+    }
+  }, [quote]);
+  if (loading) {
+    return <h2 className="quote">Loading ...</h2>;
+  }
+  if (error) {
+    return <h2 className="quote">Something went wrong, please try again</h2>;
+  }
   return (
     <section className="quote">
-      <h2 className="quote">This is some randomly jenerated quote that fetched from an external API</h2>
+      <h2 className="quote">{quote}</h2>
     </section>
   );
 }
