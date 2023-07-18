48e9f23a233433476fddefe83d713dad6240b047
Mahdi Noori <mahdinoori2389@gmail.com>
2023-07-19 02:04:18 +0430

    Add home.js file to the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
 import Calculator from './components/calculator';
 import Quote from './components/displayQuote';
+import Home from './components/home';
 
 function App() {
   return (
     <div className="container">
-      <Calculator />
-      <Quote />
+      <nav className="navbar">
+        <ul>
+          <li><NavLink to="home">Home</NavLink></li>
+          <li><NavLink to="calculator">Calculator</NavLink></li>
+          <li>
+            <NavLink to="quote">Quote</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="calculator" element={<Calculator />} />
+        <Route path="quote" element={<Quote />} />
+      </Routes>
     </div>
   );
 }
