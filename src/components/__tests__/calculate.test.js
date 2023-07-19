a5286125e572f0c11d19d1e9e249fe5fd42b5433
DevaWinner <winneraaniekan@gmail.com>
2023-07-19 14:36:41 +0100

    Add test to add two numbers

--- src/components/__tests__/calculate.test.js
+++ src/components/__tests__/calculate.test.js
@@ -6,4 +6,10 @@ describe('calculagte()', () => {
     const result = calculate(data, 'AC');
     expect(result).toEqual({ total: null, next: null, operation: null });
   });
+
+  test('should add two numbers', () => {
+    const data = { total: '10', next: '5', operation: '+' };
+    const result = calculate(data, '=');
+    expect(result).toEqual({ total: '15', next: null, operation: null });
+  });
 });
