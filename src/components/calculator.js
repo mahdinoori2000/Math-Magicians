9bb1cb880332f25dd24905c71c108dc6e14fd341
Mahdi Noori <mahdinoori2389@gmail.com>
2023-07-19 11:23:55 +0430

    Add text to calculator section

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -9,36 +9,39 @@ export default function Calculator() {
     setData(newData);
   };
   return (
-    <section className="calculator">
-      <input className="number-input" type="text" value={data.next || data.total || '0'} readOnly />
-      <div className="first-row">
-        <button type="button" onClick={() => handleClick('AC')}>AC</button>
-        <button type="button" onClick={() => handleClick('+/-')}>+/-</button>
-        <button type="button" onClick={() => handleClick('%')}>%</button>
-        <button type="button" onClick={() => handleClick('÷')}>÷</button>
-      </div>
-      <div className="second-row">
-        <button type="button" onClick={() => handleClick('7')}>7</button>
-        <button type="button" onClick={() => handleClick('8')}>8</button>
-        <button type="button" onClick={() => handleClick('9')}>9</button>
-        <button type="button" onClick={() => handleClick('x')}>x</button>
-      </div>
-      <div className="third-row">
-        <button type="button" onClick={() => handleClick('4')}>4</button>
-        <button type="button" onClick={() => handleClick('5')}>5</button>
-        <button type="button" onClick={() => handleClick('6')}>6</button>
-        <button type="button" onClick={() => handleClick('-')}>-</button>
-      </div>
-      <div className="third-row">
-        <button type="button" onClick={() => handleClick('1')}>1</button>
-        <button type="button" onClick={() => handleClick('2')}>2</button>
-        <button type="button" onClick={() => handleClick('3')}>3</button>
-        <button type="button" onClick={() => handleClick('+')}>+</button>
-      </div>
-      <div className="fifth-row">
-        <button type="button" onClick={() => handleClick('0')}>0</button>
-        <button type="button" onClick={() => handleClick('.')}>.</button>
-        <button type="button" onClick={() => handleClick('=')}>=</button>
+    <section className="calculator-section">
+      <h2 className="secondry-header">Let&apos;s do some Math!</h2>
+      <div className="calculator">
+        <input className="number-input" type="text" value={data.next || data.total || '0'} readOnly />
+        <div className="first-row">
+          <button type="button" onClick={() => handleClick('AC')}>AC</button>
+          <button type="button" onClick={() => handleClick('+/-')}>+/-</button>
+          <button type="button" onClick={() => handleClick('%')}>%</button>
+          <button type="button" onClick={() => handleClick('÷')}>÷</button>
+        </div>
+        <div className="second-row">
+          <button type="button" onClick={() => handleClick('7')}>7</button>
+          <button type="button" onClick={() => handleClick('8')}>8</button>
+          <button type="button" onClick={() => handleClick('9')}>9</button>
+          <button type="button" onClick={() => handleClick('x')}>x</button>
+        </div>
+        <div className="third-row">
+          <button type="button" onClick={() => handleClick('4')}>4</button>
+          <button type="button" onClick={() => handleClick('5')}>5</button>
+          <button type="button" onClick={() => handleClick('6')}>6</button>
+          <button type="button" onClick={() => handleClick('-')}>-</button>
+        </div>
+        <div className="third-row">
+          <button type="button" onClick={() => handleClick('1')}>1</button>
+          <button type="button" onClick={() => handleClick('2')}>2</button>
+          <button type="button" onClick={() => handleClick('3')}>3</button>
+          <button type="button" onClick={() => handleClick('+')}>+</button>
+        </div>
+        <div className="fifth-row">
+          <button type="button" onClick={() => handleClick('0')}>0</button>
+          <button type="button" onClick={() => handleClick('.')}>.</button>
+          <button type="button" onClick={() => handleClick('=')}>=</button>
+        </div>
       </div>
     </section>
   );
